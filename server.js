e683756e85d4621f328eb9f2ff6e61a944889f54
shainaveloz <shaina.veloz@gmail.com>
2016-06-25 21:25:33 -0400

    added routes

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app);
+
+app.get('/', function(req, res){
+    //res.send("Welcome to the Friend Finder Page!")
+    res.sendFile(path.join(__dirname + '/app/public/home.html'));
+});
+
+app.get('/Survey', function(req, res){
+    //res.send("Welcome to the Friend Finder Page!")
+    res.sendFile(path.join(__dirname + '/app/public/survey.html'));
+});
+
+app.listen(PORT, function(){
+    console.log('App listening on PORT ' + PORT);
+});
+
