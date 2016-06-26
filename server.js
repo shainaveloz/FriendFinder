f441c0aeb0a709f8bfe0afa4911cc6700d545715
shainaveloz <shaina.veloz@gmail.com>
2016-06-26 00:09:52 -0400

    added dropdown button

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/routing/api-routes.js')(app);
+// require('./app/routing/api-routes.js')(app);
 
 app.get('/', function(req, res){
     //res.send("Welcome to the Friend Finder Page!")
